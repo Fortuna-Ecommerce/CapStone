615ccecef3955a489651eb985def12e74cefdc01
VictorJP <victor.jaimes.puente@gmail.com>
2020-05-26 10:46:00 -0500

    updating tree, working on admin login

--- src/main/java/com/capstone/ecommerce/controllers/HomeController.java
+++ src/main/java/com/capstone/ecommerce/controllers/HomeController.java
@@ -1,8 +1,6 @@
 package com.capstone.ecommerce.controllers;
 
-import com.capstone.ecommerce.model.Product;
-import com.capstone.ecommerce.repositories.ProductRepository;
-import com.capstone.ecommerce.repositories.UserRepository;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
