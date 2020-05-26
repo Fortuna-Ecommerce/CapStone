c7d7d6b2c8d78023939827e69b3635d81d4bf9d6
Ruben <rubentrevino95@gmail.com>
2020-05-26 12:06:56 -0500

    deleting ex.prop.

--- src/main/java/com/capstone/ecommerce/controllers/HomeController.java
+++ src/main/java/com/capstone/ecommerce/controllers/HomeController.java
@@ -1,7 +1,14 @@
 package com.capstone.ecommerce.controllers;
 
+import com.capstone.ecommerce.model.Product;
+import com.capstone.ecommerce.repositories.ProductRepository;
+import com.capstone.ecommerce.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
