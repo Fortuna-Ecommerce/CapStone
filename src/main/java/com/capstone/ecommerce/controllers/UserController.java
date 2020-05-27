b38f2c11fd1e48dff94b263e1ed0c66ed7f1354f
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-05-27 01:55:14 -0500

    started setting up the user controller

--- src/main/java/com/capstone/ecommerce/controllers/UserController.java
+++ src/main/java/com/capstone/ecommerce/controllers/UserController.java
@@ -1,4 +1,73 @@
 package com.capstone.ecommerce.controllers;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class UserController {
+
+    public UserController() {
+    }
+
+    @GetMapping("/profile")
+    @ResponseBody
+    public String User() {
+        return "/profile";
+    }
+
+    @GetMapping("/user/{id}/profile/edit")
+    @ResponseBody
+    public String editProfile(){
+        return "/user/{id}/profile/edit";
+    }
+
+    @GetMapping("/user/profile/{id}")
+    @ResponseBody
+    public String viewProfile(){
+        return "user/profile/{id}";
+    }
+
+    @GetMapping("/user/products/tshirts/{id}/review")
+    @ResponseBody
+    public String productReview(){
+        return "/user/products/tshirts/{id}/review";
+    }
+
+    @GetMapping("/user/products")
+    @ResponseBody
+    public String products(){
+        return "/user/products";
+    }
+
+    @GetMapping("/user/products/tshirts/{id}")
+    @ResponseBody
+    public String sortTshirtsById(){
+        return "/user/products/tshirts/{id}";
+    }
+
+    @GetMapping("/user/products/tshirts/price/{id}")
+    @ResponseBody
+    public String sortTshirtsByPrice(){
+        return "/user/products/tshirts/price/{id}";
+    }
+
+    @GetMapping("/user/products/tshirts/category/{id}")
+    @ResponseBody
+    public String sortTshirtsByCategory(){
+        return "/user/products/tshirts/category/{id}";
+    }
+
+    @GetMapping("/user/products/tshirts/reviews")
+    @ResponseBody
+    public String sortTshirtsByReviews(){
+        return "/user/products/tshirts/reviews";
+    }
+
+    @GetMapping("/user/products/tshirts")
+    @ResponseBody
+    public String viewAllTshirts(){
+        return "/user/products/tshirts";
+    }
+
 }
