e4889ffa0447d95302c02f553d60ec64e5a92ca4
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-05-28 09:23:30 -0500

    doing a general save

--- src/main/java/com/capstone/ecommerce/controllers/ProductsController.java
+++ src/main/java/com/capstone/ecommerce/controllers/ProductsController.java
@@ -2,7 +2,6 @@ package com.capstone.ecommerce.controllers;
 
 import com.capstone.ecommerce.model.Product;
 import com.capstone.ecommerce.repositories.ProductRepository;
-import com.capstone.ecommerce.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +14,7 @@ public class ProductsController {
     private ProductRepository productRepo;
 //  CONSTRUCTOR
     public ProductsController(ProductRepository productRepo) {
+
         this.productRepo = productRepo;
     }
 
---
+++ src/main/java/com/capstone/ecommerce/controllers/UserController.java
@@ -0,0 +1,62 @@
+package com.capstone.ecommerce.controllers;
+
+import com.capstone.ecommerce.model.User;
+import com.capstone.ecommerce.repositories.UserRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+
+public class UserController {
+    private UserRepository userRepo;
+
+    //Constructor
+    public UserController(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping("/profile")
+    @ResponseBody
+    public String userProfile(Model model) {
+        Optional<User> users = userRepo.findById();
+        model.addAttribute("allUsers", users);
+        return "/user/profile";
+    }
+
+    @GetMapping("/user/{id}/profile/create")
+    @ResponseBody
+    public String userProfileCreate(Model model){
+        model.addAttribute("allUsers", userRepo.findAll());
+        return "/user/{id}/profile/create";
+    }
+
+    @GetMapping("/user/{id}/profile/edit")
+    @ResponseBody
+    public String userProfileEdit(Model model){
+        model.addAttribute("allUsers", userRepo.findAll());
+        return "/user/{id}/profile/edit";
+    }
+
+    @GetMapping("/user/profile/{id}")
+        @ResponseBody
+    public String viewUserProfile(Model model){
+        model.addAttribute("allUsers", userRepo.findAll());
+        return "/user/profile/{id}";
+        }
+
+    @GetMapping("/user/products/tshirts/{id}/review")
+    @ResponseBody
+    public String viewReviewsSubmitted(Model model){
+        model.addAttribute("allUsers", userRepo.findAll());
+        return "/user/products/tshirts/{id}/review";
+    }
+
+    @GetMapping("/user/products/tshirts")
+    @ResponseBody
+    public String viewTshirts(Model model){
+        model.addAttribute("allProducts", productRepo.findAll());
+        return "/user/products/tshirts";
+    }
+
+}
