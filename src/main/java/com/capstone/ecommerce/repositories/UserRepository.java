f06e9cc1e4c399655effd77625b538b025409631
VictorJP <victor.jaimes.puente@gmail.com>
2020-06-08 13:39:30 -0500

    fixed user controller

--- src/main/java/com/capstone/ecommerce/repositories/UserRepository.java
+++ src/main/java/com/capstone/ecommerce/repositories/UserRepository.java
@@ -11,13 +11,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(long id);
 
     //Represents the "DAO" and how to access the information on the object
-    interface UserRepository extends JpaRepository<User, Long> {
         User findByUsername(String username);
 
         void save(List<User> users);
 
     }
-}
+
 
 //    @Query("SELECT p FROM User p WHERE p.id LIKE %?1%")
 //        List<Product> findByNameContaining(String keyword);
