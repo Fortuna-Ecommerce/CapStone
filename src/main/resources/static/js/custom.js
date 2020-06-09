0a9c1a3446857cc4d8a36a7de7417ee0ac16aeff
Jeremy Telhiard <jeremy.telhiard@gmail.com>
2020-06-09 13:47:39 -0500

    Save work

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -124,34 +124,35 @@
     //     event.preventDefault();
     // }, true);
 
-    $('#buttonSubmit').on('click', function(event) {
-        console.log(test);
-        var formElement = this;
-        // You can directly create form data from the form element
-        // (Or you could get the files from input element and append them to FormData as we did in vanilla javascript)
-        var formData = new FormData(formElement);
+    // $('#buttonSubmit').on('click', function(event) {
+    //     console.log(test);
+    //     var formElement = this;
+    //     // You can directly create form data from the form element
+    //     // (Or you could get the files from input element and append them to FormData as we did in vanilla javascript)
+    //     var formData = new FormData(formElement);
+    //
+    //     $.ajax({
+    //         type: "POST",
+    //         enctype: 'multipart/form-data',
+    //         url: "/uploadFile",
+    //         data: formData,
+    //         processData: false,
+    //         contentType: false,
+    //         success: function (response) {
+    //             console.log(response);
+    //             // process response
+    //         },
+    //         error: function (error) {
+    //             console.log(error);
+    //             // process error
+    //         }
+    //     });
+    //
+    //     event.preventDefault();
+    // });
 
-        $.ajax({
-            type: "POST",
-            enctype: 'multipart/form-data',
-            url: "/uploadFile",
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function (response) {
-                console.log(response);
-                // process response
-            },
-            error: function (error) {
-                console.log(error);
-                // process error
-            }
-        });
 
-        event.preventDefault();
-    });
-
-
+    // document.getElementById('addProduct').addEventListener('change', function(event) {
 
 
 
