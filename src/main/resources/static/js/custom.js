1362120cdbde1008d81942f9d71082388315d0ab
Ruben <rubentrevino95@gmail.com>
2020-06-09 19:34:58 -0500

    added courasel

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,5 +1,9 @@
 (function() {
     "use strict";
+
+
+
+
         // Create a Stripe client.
         var stripe = Stripe('pk_test_B1PgkqwpndJUHJCdlY0I9leL00c395TbE5');
 
@@ -146,100 +150,9 @@
 
         event.preventDefault();
     });
+
+
+
+
+
 })();
-
-//COURASEL
-const slides=document.querySelector(".slider").children;
-const prev=document.querySelector(".prev");
-const next=document.querySelector(".next");
-const indicator=document.querySelector(".indicator");
-let index=0;
-
-
-prev.addEventListener("click",function(){
-    prevSlide();
-    updateCircleIndicator();
-    resetTimer();
-});
-
-next.addEventListener("click",function(){
-    nextSlide();
-    updateCircleIndicator();
-    resetTimer();
-
-});
-
-// create circle indicators
-function circleIndicator(){
-    for(let i=0; i< slides.length; i++){
-        const div=document.createElement("div");
-        div.innerHTML=i+1;
-        div.setAttribute("onclick","indicateSlide(this)")
-        div.id=i;
-        if(i==0){
-            div.className="active";
-        }
-        indicator.appendChild(div);
-    }
-}
-circleIndicator();
-
-function indicateSlide(element){
-    index=element.id;
-    changeSlide();
-    updateCircleIndicator();
-    resetTimer();
-}
-
-function updateCircleIndicator(){
-    for(let i=0; i<indicator.children.length; i++){
-        indicator.children[i].classList.remove("active");
-    }
-    indicator.children[index].classList.add("active");
-}
-
-function prevSlide(){
-    if(index==0){
-        index=slides.length-1;
-    }
-    else{
-        index--;
-    }
-    changeSlide();
-}
-
-function nextSlide(){
-    if(index==slides.length-1){
-        index=0;
-    }
-    else{
-        index++;
-    }
-    changeSlide();
-}
-
-function changeSlide(){
-    for(let i=0; i<slides.length; i++){
-        slides[i].classList.remove("active");
-    }
-
-    slides[index].classList.add("active");
-}
-
-function resetTimer(){
-    // when click to indicator or controls button
-    // stop timer
-    clearInterval(timer);
-    // then started again timer
-    timer=setInterval(autoPlay,4000);
-}
-
-
-function autoPlay(){
-    nextSlide();
-    updateCircleIndicator();
-}
-autoPlay();
-
-let timer=setInterval(autoPlay,4000);
-
