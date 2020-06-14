97789f1ad5c6a871e50640406d64e3900784e276
VictorJP <victor.jaimes.puente@gmail.com>
2020-06-14 12:54:56 -0500

    removed transaction_product

--- src/main/java/com/capstone/ecommerce/model/OrdersItem.java
+++ src/main/java/com/capstone/ecommerce/model/OrdersItem.java
@@ -16,6 +16,14 @@ public class OrdersItem {
     @JoinColumn(name = "transaction_id")
     Transaction transaction;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @ManyToOne
             (cascade = {
             CascadeType.MERGE
