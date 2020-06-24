83a4cfdf6c4425241b5233258dfd7897e7f43fec
Jeremy Telhiard <jeremy.telhiard@gmail.com>
2020-06-24 15:54:19 -0500

    Profile transactions mostly done

--- src/main/java/com/capstone/ecommerce/controllers/UserController.java
+++ src/main/java/com/capstone/ecommerce/controllers/UserController.java
@@ -1,11 +1,9 @@
 package com.capstone.ecommerce.controllers;
-import com.capstone.ecommerce.model.ShoppingCart;
+import com.capstone.ecommerce.model.*;
 
-import com.capstone.ecommerce.model.Address;
-
-import com.capstone.ecommerce.model.User;
-import com.capstone.ecommerce.model.UserWithRoles;
 import com.capstone.ecommerce.repositories.AddressRepository;
+import com.capstone.ecommerce.repositories.TransactionProductRepository;
+import com.capstone.ecommerce.repositories.TransactionRepository;
 import com.capstone.ecommerce.repositories.UserRepository;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -27,6 +25,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -39,16 +38,23 @@ public class UserController {
     private UserRepository users;
     private PasswordEncoder passwordEncoder;
     private AddressRepository addressRepo;
+    private TransactionRepository transactionRepo;
+    private TransactionProductRepository transProdRepo;
+
 
     private final String userNameRegex="[A-Za-z0-9]+";
     private final Pattern usernamePattern=Pattern.compile(userNameRegex);
 
     //Stores info in variable so it can be used elsewhere, allows information to be malleable
 
-    public UserController(UserRepository users, PasswordEncoder passwordEncoder, AddressRepository addyRepo) {
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder,
+                          AddressRepository addyRepo, TransactionRepository transactionRepo,
+                          TransactionProductRepository transProdRepo) {
         this.users = users;
         this.passwordEncoder = passwordEncoder;
         this.addressRepo = addyRepo;
+        this.transactionRepo = transactionRepo;
+        this.transProdRepo = transProdRepo;
     }
 
     @GetMapping("/register")
@@ -120,6 +126,51 @@ public class UserController {
             bill_address = new Address();
             bill_address.setStreet2("");
         }
+
+        ArrayList<TransactionTieup> finalSend = new ArrayList<>();
+        List<Transaction> baseTransactions = transactionRepo.findByUser(profileOwner);
+        List<Transactions_Product> transProds = transProdRepo.findAll();
+        List<String> colors = new ArrayList<>();
+        for(Transaction transaction : baseTransactions){
+            List<Long> quantities = new ArrayList<>();
+            List<Product> products = new ArrayList<>();
+            TransactionTieup saveFull = new TransactionTieup();
+            saveFull.setId(transaction.getId());
+            saveFull.setCreated_at(transaction.getCreated_at());
+            saveFull.setModified_at(transaction.getModified_at());
+            saveFull.setStripeId(transaction.getStripeTransID());
+            saveFull.setStatus(transaction.getTransactionStatus());
+            saveFull.setType(transaction.getTransactionType());
+            saveFull.setUsername(transaction.getUser().getUsername());
+            saveFull.setEmail(transaction.getUser().getEmail());
+            saveFull.setStripeCustomer(transaction.getUser().getStripeToken());
+            saveFull.setShipping(addressRepo.findByUserAndAddresstype(transaction.getUser(), "Shipping"));
+            saveFull.setBilling(addressRepo.findByUserAndAddresstype(transaction.getUser(), "Billing"));
+            saveFull.setTotal(transaction.getFinalAmount());
+            for(Transactions_Product transproduct : transProds){
+                if(transaction == transproduct.getTransaction()){
+                    quantities.add(transproduct.getQuantity());
+                    products.add(transproduct.getProduct());
+                    String color = "";
+                    if(transproduct.getProduct().getColor().equals("FFFFFF")){
+                        color = "White";
+                    } else if (transproduct.getProduct().getColor().equals("000000")){
+                        color = "Black";
+                    } else if (transproduct.getProduct().getColor().equals("808080")){
+                        color = "Grey";
+                    }
+                    colors.add(color);
+                }
+            }
+            saveFull.setQuantity(quantities);
+            saveFull.setProduct(products);
+            finalSend.add(saveFull);
+        }
+
+
+        model.addAttribute("transactions", finalSend);
+        model.addAttribute("colors", colors);
+
         model.addAttribute("bill_address", bill_address);
         model.addAttribute("ship_address", ship_address);
         return "users/profile";
