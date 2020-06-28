02571298e4d4fb83e1a60ab1f833df4edfd336d5
Jeremy Telhiard <jeremy.telhiard@gmail.com>
2020-06-27 22:08:13 -0500

    Tweak something to push

--- src/main/java/com/capstone/ecommerce/controllers/ErrorController.java
+++ src/main/java/com/capstone/ecommerce/controllers/ErrorController.java
@@ -18,24 +18,4 @@ class MyErrorController implements ErrorController {
         return null;
     }
 
-//    @Override
-//    public String getErrorPath() {
-//        return "/error";
-//    }
-//    @RequestMapping("/error")
-//    public String handleError(HttpServletRequest request) {
-//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-//
-//        if (status != null) {
-//            int statusCode = Integer.parseInt(status.toString());
-//
-//            if(statusCode == HttpStatus.NOT_FOUND.value()) {
-//                return "error-404";
-//            }
-//            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-//                return "error-500";
-//            }
-//        }
-//        return "error";
-//    }
 }
